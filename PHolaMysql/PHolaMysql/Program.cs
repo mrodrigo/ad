43f831aae84ae3febec251439f3b156fe6f35186
Miguel Rodrigo <mrodrigo.dam@gmail.com>
2014-09-19 09:23:55 +0200

    Subiendo Hola MySQL

---
+++ PHolaMysql/PHolaMysql/Program.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace PHolaMysql
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+
+
+			MySqlConnection mySqlConnection = new MySqlConnection (
+				"Server=localhost;Database=dbprueba;User ID=root;Password=sistemas");
+
+			mySqlConnection.Open ();
+
+			MySqlCommand mySqlCommand = mySqlConnection.CreateCommand ();
+		//	mySqlCommand.CommandText = 
+		//		string.Format ("insert into categoria (nombre) values ('{0}')", DateTime.Now);
+		// mySqlCommand.ExecuteNonQuery ();
+			mySqlCommand.CommandText = "select * from categoria";
+
+			MySqlDataReader mySqlDataReader = mySqlCommand.ExecuteReader ();
+
+			Console.WriteLine ("FieldCount={0}", mySqlDataReader.FieldCount);
+			for (int index = 0; index < mySqlDataReader.FieldCount; index++)
+				Console.WriteLine("Colum {0}={1}", index, mySqlDataReader.GetName (index));
+			mySqlConnection.Close ();
+		}
+	}
+}
