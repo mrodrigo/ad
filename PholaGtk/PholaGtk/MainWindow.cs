043029b2b8de05ecdf865aaa0673dec956fa00b7
Miguel Rodrigo <mrodrigo.dam@gmail.com>
2014-09-22 10:16:23 +0200

    Utilizando GTK

---
+++ PholaGtk/PholaGtk/MainWindow.cs
@@ -0,0 +1,23 @@
+using System;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{	
+	public MainWindow (): base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void OnButton1Clicked (object sender, EventArgs e)
+	{
+
+		label2.Text = entry1.Text;
+	}
+	
+}
