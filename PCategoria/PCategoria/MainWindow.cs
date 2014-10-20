6605462b3478ee12a1254c7b225d87132d5f2175
Miguel Rodrigo <mrodrigo.dam@gmail.com>
2014-10-20 09:35:47 +0200

    utilizando Pserpis como referencia

--- PCategoria/PCategoria/MainWindow.cs
+++ PCategoria/PCategoria/MainWindow.cs
@@ -1,51 +1,111 @@
+using Gtk;
 using System;
 using System.Data;
-using Gtk;
-using MySql.Data;
-using MySql.Data.MySqlClient;
 
+using PCategoria;
+using SerpisAd;
 
 public partial class MainWindow: Gtk.Window
 {	
+	private IDbConnection dbConnection;
 	private ListStore listStore;
 
-	private MySqlConnection mySqlConnection;
 	public MainWindow (): base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
-		treeView.AppendColumn("id", new CellRendererText(), "text", 0);
-		treeView.AppendColumn("nombre", new CellRendererText(), "text", 1);
 
-		listStore = new ListStore(typeof(string), typeof(string));
+		deleteAction.Sensitive = false;
+		editAction.Sensitive = false;
 
+		dbConnection = App.Instance.DbConnection;
+
+		treeView.AppendColumn ("id", new CellRendererText (), "text", 0);
+		treeView.AppendColumn ("nombre", new CellRendererText (), "text", 1);
+		listStore = new ListStore (typeof(ulong), typeof(string));
 		treeView.Model = listStore;
-		mySqlConnection = new MySqlConnection (
-			"Server=localhost;Database=dbprueba;User ID=root;Password=sistemas");
-		mySqlConnection.Open ();
 
-		MySqlCommand mySqlCommand = mySqlConnection.CreateCommand ();
-		mySqlCommand.CommandText = "select * from categoria";
+		fillListStore ();
 
-		MySqlDataReader mySqlDataReader = mySqlCommand.ExecuteReader ();
+		treeView.Selection.Changed += selectionChanged;
+	}
 
-		while (mySqlDataReader.Read()) {
-			object id = mySqlDataReader ["id"].ToString();
-			object nombre = mySqlDataReader ["nombre"];
+	private void selectionChanged (object sender, EventArgs e) {
+		Console.WriteLine ("selectionChanged");
+		bool hasSelected = treeView.Selection.CountSelectedRows () > 0;
+		deleteAction.Sensitive = hasSelected;
+		editAction.Sensitive = hasSelected;
+	}
+
+	private void fillListStore() {
+		IDbCommand dbCommand = dbConnection.CreateCommand ();
+		dbCommand.CommandText = "select * from categoria";
+
+		IDataReader dataReader = dbCommand.ExecuteReader ();
+		while (dataReader.Read()) {
+			object id = dataReader ["id"];
+			object nombre = dataReader ["nombre"];
 			listStore.AppendValues (id, nombre);
 		}
-		mySqlDataReader.Close ();
+		dataReader.Close ();
 	}
 
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
 	{
+		dbConnection.Close ();
 		Application.Quit ();
 		a.RetVal = true;
 	}
+
 	protected void OnAddActionActivated (object sender, EventArgs e)
 	{
-		//listStore.AppendValues (id, nombre);
+		string insertSql = string.Format(
+			"insert into categoria (nombre) values ('{0}')",
+			"Nuevo " + DateTime.Now
+		);
+		Console.WriteLine ("insertSql={0}", insertSql);
+		IDbCommand dbCommand = dbConnection.CreateCommand ();
+		dbCommand.CommandText = insertSql;
+
+		dbCommand.ExecuteNonQuery ();
 	}
 
+	protected void OnRefreshActionActivated (object sender, EventArgs e)
+	{
+		listStore.Clear ();
+		fillListStore ();
+	}
 
+	protected void OnDeleteActionActivated (object sender, EventArgs e)
+	{
+		MessageDialog messageDialog = new MessageDialog (
+			this,
+			DialogFlags.Modal,
+			MessageType.Question,
+			ButtonsType.YesNo,
+			"¿Quieres eliminar el registro?"
+		);
+		messageDialog.Title = Title;
+		ResponseType response = (ResponseType) messageDialog.Run ();
+		messageDialog.Destroy ();
 
+		if (response != ResponseType.Yes)
+			return;
+
+		TreeIter treeIter;
+		treeView.Selection.GetSelected (out treeIter);
+		object id = listStore.GetValue (treeIter, 0);
+		string deleteSql = string.Format ("delete from categoria where id={0}", id);
+		IDbCommand dbCommand = dbConnection.CreateCommand ();
+		dbCommand.CommandText = deleteSql;
+
+		dbCommand.ExecuteNonQuery ();
+	}
+
+	protected void OnEditActionActivated (object sender, EventArgs e)
+	{
+		TreeIter treeIter;
+		treeView.Selection.GetSelected (out treeIter);
+		object id = listStore.GetValue (treeIter, 0);
+		CategoriaView categoriaView = new CategoriaView (id);
+	}
 }
