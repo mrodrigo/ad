a08a83ac6004533186fb6d3c870c37e0406a01ce
Miguel Rodrigo <mrodrigo.dam@gmail.com>
2015-01-23 00:10:31 +0100

    Ejercicios gradle

--- JHibernateCategoria/src/main/java/serpis/ad/Categoria.java
+++ JHibernateCategoria/src/main/java/serpis/ad/Categoria.java
@@ -1,17 +1,34 @@
 package serpis.ad;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
+import org.hibernate.annotations.GenericGenerator;
+
+
+@Entity
 public class Categoria {
 	private Long id;
 	private String nombre;
 	
-	 public String getTitle() {
-		return title;
-	 }
+	@Id
+	@GeneratedValue(generator="increment")
+	@GenericGenerator(name="increment", strategy = "increment")
+	public Long getId() {
+		return id;
+	}
+	 
+	private void setId(Long id){
+		 this.id = id;
+	}
 
-    public void setTitle(String nombre) {
-		this.nombre = nombre;
+    public String getNombre() {
+		return nombre;
     }
+    
+	public void setNombre(String nombre){
+		 this.nombre = nombre;
+	}
 
 }
